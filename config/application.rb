1b9c6f449f1f6688db19fdbc7296374e0b45835d
Fabio Coloni <fcoloni@gmail.com>
2014-08-09 21:10:01 -0300

    ...

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,19 @@ Bundler.require(*Rails.groups)
 
 module Vendoaalma
   class Application < Rails::Application
+    
+    config.to_prepare do
+      # Load application's model / class decorators
+      Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
+        Rails.configuration.cache_classes ? require(c) : load(c)
+      end
+
+      # Load application's view overrides
+      Dir.glob(File.join(File.dirname(__FILE__), "../app/overrides/*.rb")) do |c|
+        Rails.configuration.cache_classes ? require(c) : load(c)
+      end
+    end
+
     I18n.enforce_available_locales = false
     config.to_prepare do
       # Load application's model / class decorators
